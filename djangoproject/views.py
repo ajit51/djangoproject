85e54a12fd0e13889fff1667ab54ec7c0d45c879
ajit51 <ajitk433@gmail.com>
2022-06-20 17:57:15 +0530

    send data dynamicall from views to template (html page)

--- djangoproject/views.py
+++ djangoproject/views.py
@@ -6,7 +6,28 @@ def home(request):
     date = datetime.datetime.now()
     print("test function is called from views")
     #return HttpResponse("<h1> Hello this is index page </h1>" +str(date))
-    return render(request, "home.html", {})
+    isActive = True
+    name = "Ajit Kumar"
+    list_of_programs = [
+        "WAP to check even or odd",
+        "WAP to check prime number",
+        "WAP to print all prime number from 1 to 100",
+        "WAP to print Pascal tringle"
+    ]
+    student = {
+        "student_name" : "Rahul",
+        "student_college" : "XYZ",
+        "student_city" : "Mumbai"
+    }
+
+    data = {
+            'date' : date,
+            'isActive' : isActive,
+            'name' : name,
+            'list_of_programs' : list_of_programs,
+            'student_data' : student
+            }
+    return render(request, "home.html", data)
 
 def about(request):
     #return HttpResponse("<h1> Hello this is about page </h1>")
